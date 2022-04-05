ffc3fefe40ce7a2223480e97dd6199c94fe71e57
Amoghraj <amogh@ajnalens.com>
2022-04-05 20:46:56 +0530

    changes trail

--- src/ui/projects/CreateProjectPage.js
+++ src/ui/projects/CreateProjectPage.js
@@ -123,7 +123,7 @@ export default function CreateProjectPage({ history, location }) {
                 </ProjectGridHeaderRow>
                 <ProjectGridHeaderRow>
                   <Button as={Link} to="/scenes/new">
-                    Import From Blender
+                    Import From Blender modified
                   </Button>
                   <Button as={Link} to="/projects/new">
                     New Empty Project
